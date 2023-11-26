8dabdd967efa8c1b3317586446307728e352a3d8
Walt25 <trantienn25@gmail.com>
2023-11-27 05:50:32 +0700

    commit main (navbar + footer + default layout)

---
+++ views/main/footer.php
@@ -0,0 +1,108 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+
+  <!-- Section: Links  -->
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 mt-5">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem"></i>BIG FARM
+          </h6>
+          <p>
+            <span class="fw-bold fst-italic">FASHION FOR ALL</span>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 mt-5">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Áo thun</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Áo sơ mi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Áo khoác</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Váy </a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 mt-5">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            News
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Sale</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Private Product</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 mt-5">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home"></i>Address: KTX Khu A</p>
+          <p>
+            <i class="fas fa-envelope"></i>Email: bigfarm@gmail.com</p>
+          <p><i class="fas fa-phone"></i>Hotline: 01 234 567 88</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+      <div class="text-center">
+        <h4>© 2022 - CÔNG TY TNHH BIGFARM VN </h4>
+
+      Giấy CNĐKDN: 0310874914 – Ngày cấp: 25/11/2011 - Cơ quan cấp: Phòng Đăng Ký Kinh Doanh – Sở Kế Hoạch và Đầu Tư TP.HCM
+
+      Địa chỉ đăng ký kinh doanh: 766/3B-3C Sư Vạn Hạnh (Nối dài), Phường 12, Quận 10, TP.HCM - Điện thoại: (028) 3868 4857 - Mua hàng: (028) 7307 1441 - Email: cskh@yame.vn
+      </div>
+    </div>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-2 fw-bold" style="background-color: rgba(0, 0, 0, 0.05);">
+    <a href="index.php?page=main&controller=layouts&action=index"><span class="red">BIG</span><span class="green">FARM</span> 
+  </div>
+  <!-- Copyright -->
+</footer>
+ 
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+<!-- Footer -->
+  <script type="text/javascript">
+     $(function(){
+        $(".navbar-toggler").click(function(e){
+
+          if($('.navbar-collapse').hasClass("hide")){
+            $('.navbar-collapse').removeClass('hide');
+            $('.navbar-collapse').addClass('show');
+            console.log('show');
+          }
+          else{
+            $('.navbar-collapse').removeClass('show');
+            $('.navbar-collapse').addClass('hide');     
+            console.log('hide');      
+          }
+
+        });
+     });  
+  </script>
+</body>
+</html>
