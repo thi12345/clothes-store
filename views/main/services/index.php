1f00add6c8719162aac338f27be3b275040919ea
Thi <74984774+thi12345@users.noreply.github.com>
2023-12-03 06:53:49 +0700

    Update index.php

--- views/main/services/index.php
+++ views/main/services/index.php
@@ -15,15 +15,15 @@ include_once('views/main/navbar.php');
             <div class="col-lg-4 col-md-6 text-center container">
                 <div class="fs-5 fw-bold">Tư vấn thời trang</div>
                 <p class="justify-content container">
-                    Cung cấp dịch vụ tư vấn thời trang theo nhu cầu của khách hàng và giúp
-                    khách hàng lựa chọn sản phẩm phù hợp.
+                    Dịch vụ tư vấn những sản phẩm thời trang theo nhu cầu của khách hàng 
+                    và giúp khách nắm bắt xu hướng ăn mặc một cách nhanh chóng nhất
                 </p>
             </div>
             <div class="col-lg-4 col-md-6 text-center ">
                 <div class="fs-5 fw-bold">In áo theo yêu cầu</div>
                 <p class="justify-content container">
-                    Cung cấp dịch vụ in áo lớp theo yêu cầu. Đặc biệt là in
-                    áo cho các sự kiện, cuộc thi và lễ hội.
+                    Cung cấp dịch vụ in áo theo yêu cầu với số lượng lớn
+                    cho các sự kiện, lễ hội hoặc áo lớp với chất lượng tốt và giá cả phải chăng.
                 </p>
             </div>
             <div class="col-lg-2 col-md-0"></div>
@@ -31,14 +31,14 @@ include_once('views/main/navbar.php');
             <div class="col-lg-4 col-md-6 text-center">
                 <div class="fs-5 fw-bold">Giao hàng toàn quốc</div>
                 <p class="justify-content container">
-                    Cung cấp dịch vụ giao hàng tận nơi trên khắp 63 tỉnh thành. 
+                    Giao hàng tận nơi trên khắp 63 tỉnh thành. 
                     Không tính thêm phí phụ thu nếu mua sản phẩm với số lượng lớn.
                 </p>
             </div>
             <div class="col-lg-4 col-md-6 text-center ">
-                <div class="fs-5 fw-bold">Đổi trả hàng trong một tháng</div>
+                <div class="fs-5 fw-bold">Đổi trả hàng trong một tuần</div>
                 <p class="justify-content container">
-                    Cho phép đổi trả hàng trong một tháng
+                    Cho phép đổi trả hàng trong một tuần
                     nếu tình trạng sản phẩm thỏa mãn 
                     điều khoản dịch vụ của cửa hàng.
                 </p>    
